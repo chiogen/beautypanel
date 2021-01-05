2c0bc8a4da6ebb4f06db7a8b209bf7be3b7baedd
chiogen <iwashier@gmail.com>
2021-01-05 19:04:39 +0100

    More layout

--- src/beautypanel.ts
+++ src/beautypanel.ts
@@ -6,15 +6,11 @@ const keys = Object.keys(window);
 function showLayerNames() {
     const allLayers = app.activeDocument.layers;
     const allLayerNames = allLayers.map(layer => layer.name);
-    const sortedNames = allLayerNames.sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
     document.getElementById("center").innerHTML = `
       <ul>${
-        sortedNames.map(name => `<li>${name}</li>`).join("")
+        allLayerNames.map(name => `<li>${name}</li>`).join("")
       }</ul>`;
 }
-
-document.body.addEventListener('load', () => {
-    showLayerNames();
-})
+showLayerNames();
 
 console.log(app)
