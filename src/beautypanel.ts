85d5a97454ba5815208a1f8ab439716d88b911b6
chiogen <iwashier@gmail.com>
2021-01-18 08:59:55 +0100

    Removed redundant brackets

--- src/beautypanel.ts
+++ src/beautypanel.ts
@@ -3,7 +3,7 @@ import { localizationLoaded } from './localization';
 import { app } from 'photoshop';
 import { renderApp } from './interface';
 
-localizationLoaded.then((renderApp))
+localizationLoaded.then(renderApp)
 
 app.eventNotifier = (event, descriptor) => {
     console.log(event, JSON.stringify(descriptor, null, ' '));
