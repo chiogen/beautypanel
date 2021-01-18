d2ebec1c417e350fb9f0d885790daa4bc69999b3
chiogen <iwashier@gmail.com>
2021-01-18 08:54:01 +0100

    Rm window.LayerUtils

--- src/beautypanel.ts
+++ src/beautypanel.ts
@@ -9,7 +9,6 @@ import { Tabbar } from './interface/tabbar';
 import { localizationLoaded } from './localization';
 import { Pages } from './interface/pages';
 import * as photoshop from 'photoshop';
-import { LayerUtils } from './common/layer-utils';
 
 localizationLoaded.then(() => {
     ReactDOM.render(html`
@@ -21,5 +20,3 @@ localizationLoaded.then(() => {
 photoshop.app.eventNotifier = (event, descriptor) => {
     console.log(event, JSON.stringify(descriptor, null, ' '));
 }
-
-window.LayerUtils = LayerUtils;
