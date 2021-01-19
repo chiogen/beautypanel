7a58aad36716102ec63b26054e5198752f6be311
chiogen <iwashier@gmail.com>
2021-01-19 20:37:46 +0100

    Prepare events

--- src/interface/pages/effects.tsx
+++ src/interface/pages/effects.tsx
@@ -1,4 +1,5 @@
 import i18next from 'i18next';
+import { app } from 'photoshop';
 import * as React from 'react'
 
 type Props = {
@@ -15,13 +16,68 @@ export class Effects extends React.Component<Props> {
             style.display = 'none';
         }
 
-        return <div className="page" style={style}>
+        return <div id="effects" className="page" style={style}>
             <sp-action-button>{i18next.t('effects.enhanceDetails')}</sp-action-button>
             <sp-action-button>{i18next.t('effects.strengthenDetails')}</sp-action-button>
             <sp-action-button>{i18next.t('effects.orton')}</sp-action-button>
             <sp-action-button>{i18next.t('effects.vignette')}</sp-action-button>
             <sp-action-button>{i18next.t('effects.autumn')}</sp-action-button>
-        </div>;
+        </div>
     }
 
+}
+
+async function enhanceDetails(e: React.MouseEvent<HTMLButtonElement>) {
+    try {
+
+
+
+    } catch(err) {
+        const message = err.message || err;
+        app.showAlert(message);        
+    }
+}
+
+async function strengthenDetail(e: React.MouseEvent<HTMLButtonElement>) {
+    try {
+
+        
+
+    } catch(err) {
+        const message = err.message || err;
+        app.showAlert(message);        
+    }
+}
+
+async function createOrthonEffect(e: React.MouseEvent<HTMLButtonElement>) {
+    try {
+
+        
+
+    } catch(err) {
+        const message = err.message || err;
+        app.showAlert(message);        
+    }
+}
+
+async function createVignette(e: React.MouseEvent<HTMLButtonElement>) {
+    try {
+
+        
+
+    } catch(err) {
+        const message = err.message || err;
+        app.showAlert(message);        
+    }
+}
+
+async function createAutumnEffect(e: React.MouseEvent<HTMLButtonElement>) {
+    try {
+
+        
+
+    } catch(err) {
+        const message = err.message || err;
+        app.showAlert(message);        
+    }
 }
