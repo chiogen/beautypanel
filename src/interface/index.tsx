b4e701bffdf2a136ebb80cf9e36b13f30686c8a0
Chiogen <iwashier@gmail.com>
2021-01-30 16:48:42 +0100

    Render text when no document at load

--- src/interface/index.tsx
+++ src/interface/index.tsx
@@ -6,12 +6,25 @@ import { Tabbar } from './tabbar';
 
 export function renderApp() {
     try {
-        ReactDOM.render(
-            <div key="root">
-                <Tabbar />
-                <Pages />
-            </div>
-        , document.getElementById('app'));
+
+        const rootEl = document.getElementById('app');
+
+        if (app.activeDocument) {
+            ReactDOM.render(
+                <div key="app">
+                    <Tabbar />
+                    <Pages />
+                </div>,
+                rootEl
+            );
+        } else {
+            ReactDOM.render(
+                <div key="failstart">
+                    <h3 style={{ textAlign: 'center' }}>Waiting for image...</h3>
+                </div>,
+                rootEl
+            );
+        }
     } catch (err) {
         console.error(err);
         app.showAlert(err.message);
