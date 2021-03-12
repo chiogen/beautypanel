6f59a6bdc5ed1e4d85e7b60c0e6d10729d8e66cc
Chiogen <iwashier@gmail.com>
2021-03-12 23:47:29 +0100

    Updated button styles and module types

--- src/module-types.d.ts
+++ src/module-types.d.ts
@@ -1,20 +1,37 @@
 declare namespace JSX {
+
+    type DefaultAttributes = {
+        slot?: string
+    }
+
     interface IntrinsicElements {
 
         // <sp-checkbox>
-        'sp-action-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLButtonElement> &  {
-            active?: boolean
+        'sp-action-button': React.DetailedHTMLProps<DefaultAttributes & React.HTMLAttributes<HTMLButtonElement> & {
+            quiet?: boolean
             disabled?: boolean
         }, HTMLButtonElement>
 
+        // <sp-button>
+        'sp-button': React.DetailedHTMLProps<DefaultAttributes & React.HTMLAttributes<HTMLButtonElement> &  {
+            disabled?: boolean
+            variant?: string
+            quiet?: boolean
+        }, HTMLButtonElement>
+
+        // <sp-icon>
+        'sp-icon': React.DetailedHTMLProps<DefaultAttributes & React.HTMLAttributes<HTMLElement> & {
+            size?: string
+        }, HTMLElement>
+
         // <sp-checkbox>
-        'sp-checkbox': React.DetailedHTMLProps<React.HTMLAttributes<HTMLInputElement>, HTMLInputElement>
+        'sp-checkbox': React.DetailedHTMLProps<DefaultAttributes & React.HTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
         // <preset-edit-dialog>
-        'preset-edit-dialog': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+        'preset-edit-dialog': React.DetailedHTMLProps<DefaultAttributes & React.HTMLAttributes<HTMLElement>, HTMLElement>
 
         // <preset-edit-dialog-slider>
-        'preset-edit-dialog-slider': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+        'preset-edit-dialog-slider': React.DetailedHTMLProps<DefaultAttributes & React.HTMLAttributes<HTMLElement>, HTMLElement>
 
         // <sp-slider>
         'sp-slider': React.DetailedHTMLProps<any, HTMLElement>
