88c1012aec888c73ddd2fbdd7a78222520d871b2
Chiogen <iwashier@gmail.com>
2021-04-07 18:32:48 +0200

    Removed empty moduile

--- src/common/layer-utils.ts
+++
@@ -1,7 +0,0 @@
-import { ActionDescriptor, app, Layer } from "photoshop";
-import { _selectLayers } from "../modules/image/select-layers";
-import { pixels } from "./units";
-
-export namespace LayerUtils {
-
-}
