6a814939c4837bd348a70fc096ca81632e06219a
Chiogen <iwashier@gmail.com>
2021-04-14 13:19:48 +0200

    ADd type for addNotificationListener

--- typings/photoshop.d.ts
+++ typings/photoshop.d.ts
@@ -200,6 +200,7 @@ declare module 'photoshop' {
 
     export const action: {
         readonly batchPlay: BatchPlay
+        addNotificationListener(types: Array<{ event: string }>, listener: (event: unknown) => void): void;
     };
 
 }
