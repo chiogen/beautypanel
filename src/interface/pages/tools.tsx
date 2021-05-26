df3ba03534d1d05516879976817c00f6e87d8f7a
Chiogen <iwashier@gmail.com>
2021-05-26 22:39:03 +0200

    Updated rendering

--- src/interface/pages/tools.tsx
+++ src/interface/pages/tools.tsx
@@ -1,35 +1,28 @@
-import i18next from 'i18next';
 import * as React from 'react'
-import { store, TState } from '../../store';
 import { DodgeAndBurn } from './tools/dodge-and-burn';
 import { FrequencySeparation } from './tools/frequency-separation';
 import { CurrentToolHardness } from './tools/hardness';
 import { CurrentToolOpacity } from './tools/opacity';
 import { Zoom } from './tools/zoom';
 
-type Props = {
+type P = {
     isActive: boolean
 }
-type State = {}
 
-export class Tools extends React.Component<Props, State> {
-    render() {
+export const Tools = (props: P) => {
 
-        const classes = ['page'];
-
-        let style: React.CSSProperties = {};
-        if (!this.props.isActive) {
-            style.display = 'none';
-        }
-
-        return (
-            <div id="tools" className={classes.join(' ')} style={style}>
-                <FrequencySeparation />
-                <DodgeAndBurn />
-                <CurrentToolOpacity />
-                <CurrentToolHardness />
-                <Zoom />
-            </div>
-        );
+    let style: React.CSSProperties = {};
+    if (!props.isActive) {
+        style.display = 'none';
     }
+
+    return (
+        <div id="tools" className="page" style={style}>
+            <FrequencySeparation />
+            <DodgeAndBurn />
+            <CurrentToolOpacity />
+            <CurrentToolHardness />
+            <Zoom />
+        </div>
+    );
 }
