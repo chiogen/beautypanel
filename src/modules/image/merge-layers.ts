aa7a59fe4272192f54cadd8253d826512cfb5e96
Mike Harder <mharder@stoehr-sauer.de>
2023-06-29 09:00:13 +0200

    Fixed mergeLayers

--- src/modules/image/merge-layers.ts
+++ src/modules/image/merge-layers.ts
@@ -1,32 +1,46 @@
 import { app } from 'photoshop';
+import { ActionDescriptor } from 'photoshop/dom/CoreModules';
 import { Document } from 'photoshop/dom/Document';
 import { Layer } from 'photoshop/dom/Layer';
 import { checkDescriptorError } from '../../common/errors/handle-error';
 import { selectLayers } from './select-layers';
 
-export async function mergeLayers(document: Document, layers: Layer[]): Promise<Layer> {
+export async function mergeLayers(document: Document, layers: Layer[], name?: string): Promise<Layer> {
 
-    const duplicates = await Promise.all(layers.map(async layer => layer.duplicate())) as Layer[];
+    const duplicates = await duplicateLayers(layers);
+    await selectLayers(duplicates, true);
 
-    await selectLayers(duplicates);
+    await mergeLayersNew();
+    const mergedLayer = document.activeLayers[0];
 
-    const [result] = await app.batchPlay([
-        {
-            _obj: 'mergeLayersNew'
-        }
-    ], {});
+    mergedLayer.name = name ?? 'New Merged Layer';
+    return mergedLayer;
+}
 
-    // Delete duplicates if the command didn't already do it
-    for (const layer of duplicates) {
-        try {
-            layer.delete();
-        } catch {
-            // Do nothing
-        }
+async function duplicateLayers(layers: Layer[]) {
+
+    const duplicates: Layer[] = [];
+
+    for (const layer of layers) {
+        const newLayer = await layer.duplicate();
+        if (!newLayer)
+            throw new Error(`Layer "${layer.name}" didnt get duplicated.`);
+        duplicates.push(newLayer);
     }
 
+    return duplicates;
+}
+
+
+function _mergeLayersNew() {
+    return {
+        _obj: 'mergeLayersNew',
+    } as ActionDescriptor;
+}
+async function mergeLayersNew() {
+    const [result] = await app.batchPlay([
+        _mergeLayersNew()
+    ], {});
 
     checkDescriptorError(result);
-
-    return document.activeLayers[0];
 }
